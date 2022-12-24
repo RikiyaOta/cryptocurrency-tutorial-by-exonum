700e19ec2be14d54037cd14e6e36c61aabcef7f8
rikiyaota <rikiya0421.fcb6@gmail.com>
2022-12-24 11:23:39 +0900

    Declare persistent data and create schema.

---
+++ src/lib.rs
@@ -0,0 +1,47 @@
+use exonum::merkledb::access::Access;
+use exonum_crypto::PublicKey;
+
+mod proto;
+
+const INIT_BALANCE: u64 = 100;
+
+#[derive(Serialize, Deserialize, Clone, Debug)]
+#[derive(ProtobufConvert, BinaryValue, ObjectHash)]
+#[protobuf_convert(source = "proto::Wallet")]
+pub struct Wallet {
+    pub pub_key: PublicKey,
+    pub name: String,
+    pub balance: u64,
+}
+
+impl Wallet {
+    pub fn new(&pub_key: &PublicKey, name: &str, balance: u64) -> Self {
+        Self {
+            pub_key,
+            name: name.to_owned(),
+            balance,
+        }
+    }
+
+    pub fn increase(self, amount: u64) -> Self {
+        let balance = self.balance + amount;
+        Self::new(&self.pub_key, &self.name, balance)
+    }
+
+    pub fn decrease(self, amount: u64) -> Self {
+        debug_assert!(self.balance >= amount);
+        let balance = self.balance - amount;
+        Self::new(&self.pub_key, &self.name, balance)
+    }
+}
+
+#[derive(Debug, FromAccess)]
+pub struct CurrencySchema<T: Access> {
+    pub wallets: MapIndex<T::Base, PublicKey, Wallet>
+}
+
+impl<T: Access> CurrencySchema<T> {
+    pub fn new(access: T) -> Self {
+        Self::from_root(access).unwrap()
+    }
+}
